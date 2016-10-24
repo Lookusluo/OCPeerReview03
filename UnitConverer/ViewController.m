a01a60fb85edd5d356028cd5cf8b6c331fe0b52c
lookusluo <comeluo@hotmail.com>
2016-10-24 16:49:56 -0500

    AddTheWeb Feature, fetch current currency rate; Note this source is not secure. So I need to turn off NSAppTransportSecurity by add this to info.plist

--- UnitConverer/ViewController.m
+++ UnitConverer/ViewController.m
@@ -7,10 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
 
-
-@interface ViewController () 
+@interface ViewController () <CRCurrencyRequestDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *InputField;
 
@@ -21,14 +22,16 @@
 @property (weak, nonatomic) IBOutlet UILabel *CurrencyB;
 @property (weak, nonatomic) IBOutlet UILabel *CurrencyC;
 
-
+@property (nonatomic) CRCurrencyRequest *req;
 @end
 
 @implementation ViewController
 
 - (IBAction)ConvertButton:(id)sender {
     self.ButtonTapped.enabled = NO;
-    self.CurrencyA.text = @"A";
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
 }
 
 
@@ -37,6 +40,22 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)currencyRequest:(CRCurrencyRequest *)req retrievedCurrencies:(CRCurrencyResults *)currencies {
+    self.ButtonTapped.enabled = YES;
+    
+    double inputValue = [self.InputField.text floatValue];
+    double euroValue = inputValue * currencies.EUR;
+    double ukValue = inputValue* currencies.GBP;
+    double jpValue = inputValue* currencies.JPY;
+    
+    NSString *temp1 = [NSString stringWithFormat:@"%.2f", euroValue];
+    NSString *temp2 = [NSString stringWithFormat:@"%.2f", ukValue];
+    NSString *temp3 = [NSString stringWithFormat:@"%.2f", jpValue];
+    
+    self.CurrencyA.text = temp1;
+    self.CurrencyB.text = temp2;
+    self.CurrencyC.text = temp3;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
